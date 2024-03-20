df23b740f98d4787beee6137a6d0e379cc1f2eac
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-03-20 09:39:04 -0700

    Update Trees.cpp

--- Libraries/Trees.cpp
+++ Libraries/Trees.cpp
@@ -1,1 +1,14 @@
 #include "../Headers/cs121.h"
+
+Tree::Tree()
+{
+    Left = NULL;
+    Right = NULL;
+}
+
+void Tree::push(std::string side, std::string data)
+{
+    Node* newnode = new Node(data);
+    Node* temp = root;
+
+}
